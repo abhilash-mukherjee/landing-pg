4d46e3ce10e3e65cebd66dbd9e02917e0a6ce1c6
Abhilash <abhilash301267@gmail.com>
2024-02-26 13:55:10 +0530

    Responsiveness imposed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 export const Hero = () => {
     return (
         <>
-            <div class="container lg-auto p-6 flex flex-col	items-center	self-center	flex-1 justify-center max-w-4xl	space-y-5 ">
-                <h1 className='text-3xl sm:text-3xl md:text-2xl lg:text-5xl xl:text-5xl text-white text-center'>Intuitive User Assistance For XR Apps</h1>
-                <h2 className='text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-2xl text-white text-center	'>We help XR Apps to improve their user onboarding, activation, adoption and retention with the right in-app nudges. No Coding Required.</h2>
+            <div class="container lg-auto p-6 flex flex-col	items-center	self-center	flex-1 justify-center max-w-4xl	gap-y-5 xl:gap-y-8 lg:gap-y-6 ">
+                <div className='flex flex-col gap-y-2 md:gap-y-2 sm:gap-y-1 lg:gap-y-5'>
+                    <h1 className='text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl text-white text-center font-semibold'>Intuitive User Assistance</h1>
+                    <h1 className='text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl text-white text-center font-semibold'>For XR Apps</h1>
+                </div>
+                <h2 className='text-lg sm:text-xl md:text-xl lg:text-xl xl:text-2xl text-white text-center'>We help XR Apps to improve their user onboarding, activation, adoption, and retention with the right in-app nudges. No Coding Required.</h2>
+
+                <div className='py-3'>
                 <button
-                    className="bg-transparent hover:bg-fuchsia-950 text-white font-bold py-2 px-4 rounded-full border-solid border-slate-300 border-2 transition-transform duration-100"
+                    className="bg-transparent text-white font-normal py-2 px-8 rounded-full border-solid border-slate-300 border-2 transition-transform duration-100 text-base"
                     style={{ transform: 'scale(1.0)' }}
                     onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
                     onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1.0)'}
@@ -14,6 +19,7 @@ export const Hero = () => {
                 >
                     Get in Touch!
                 </button>
+                </div>
 
 
             </div></>
