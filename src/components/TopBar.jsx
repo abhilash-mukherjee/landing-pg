1e6f2ca02b33422d89aa9bc00fc5006474e2a12b
Abhilash <abhilash301267@gmail.com>
2024-03-23 00:33:56 +0530

    Dev test

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ const TopBar = () => {
   return (
     <div className= "text-white p-6 w-full">
       <div className="container mx-auto">
-        <h1 className="text-xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-3xl text-white">penguin labs</h1>
+        <h1 className="text-xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-3xl text-white">penguin labss</h1>
       </div>
     </div>
   );
